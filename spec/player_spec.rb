127254d48d632e2b09463d9a625aaab0cee76b0b
JoannaCoPo <jococopo@gmail.com>
2021-04-07 09:43:33 -0600

    Initial commit

---
+++ spec/player_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/player'
+require 'pry'
+
+RSpec.describe Player do
+  it 'exists' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder", salary: 1_000_000})
+
+    expect(roy).to be_instance_of(Player)
+  end
+
+  it 'has attributes' do
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder", salary: 1_000_000})
+    expect(roy.name).to eq("Roy Kent")
+    expect(roy.position).to eq("Center Midfielder")
+    expect(roy.salary).to eq(1_000_000)
+  end
+
+
+
+end
